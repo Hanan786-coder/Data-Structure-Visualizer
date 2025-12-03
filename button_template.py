ed1fe47ae6f138197e441e219911267e1c2c24bb
Hanan <hananbhatti412@gmail.com>
2025-12-03 21:38:38 +0500

    Import and bug fixes

--- button_template.py
+++ button_template.py
@@ -1,22 +1,27 @@
+import pygame
+import Colors
+
 class Button:
-    def __init__(self, x, y, w, h, text, action_code):
+    def __init__(self, x, y, w, h, text, action_code, txt_size):
         self.rect = pygame.Rect(x, y, w, h)
         self.text = text
         self.action_code = action_code
         self.is_hovered = False
+        self.FONT = pygame.font.Font("ScienceGothic-Regular.ttf", txt_size)
 
     def draw(self, surface):
-        color = TEAL_BRIGHT if self.is_hovered else TEAL
+        self.check_hover(pygame.mouse.get_pos())
+        color = Colors.TEAL_BRIGHT if self.is_hovered else Colors.TEAL
         pygame.draw.rect(surface, color, self.rect, border_radius=8)
 
         # Text
-        txt_surf = font_ui.render(self.text, True, LIGHT_GREY)
+        txt_surf = self.FONT.render(self.text, True, Colors.LIGHT_GREY)
         txt_rect = txt_surf.get_rect(center=self.rect.center)
         surface.blit(txt_surf, txt_rect)
 
     def check_hover(self, pos):
         self.is_hovered = self.rect.collidepoint(pos)
 
-    def is_clicked(self, pos):
-        return self.rect.collidepoint(pos)
+    def is_clicked(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
 
